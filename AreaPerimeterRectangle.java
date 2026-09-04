4248d34050fb87661b7eae9947f2946b96d76fdd
Suchita Patil <suchita4004@gmail.com>
2026-09-04 23:46:10 +0530

    Add Area and Perimeter calculation for rectangle

---
+++ AreaPerimeterRectangle.java
@@ -0,0 +1,9 @@
+class CodeChef {
+    public static void main(String[] args) {
+        int length = 11;
+        int breadth = 13;
+
+        System.out.println(length * breadth);
+        System.out.println(2 * (length + breadth));
+    }
+}
