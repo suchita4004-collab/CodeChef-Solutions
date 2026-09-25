4268b0308a6aba412c3dd744c986fa2048221f13
Suchita Patil <suchita4004@gmail.com>
2026-09-25 21:59:32 +0530

    ✅ CLGOPJP45 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-25

---
+++ codechef/medium/CLGOPJP45/solution.cpp
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int height = 15;
+        // Update the blanks in the code below
+        if(height >10 || a < 5){
+            System.out.println(1);
+        }
+        else{
+            System.out.println(0);
+        }
+    }
+}
