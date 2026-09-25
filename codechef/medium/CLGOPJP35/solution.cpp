5032021a8f8bb6cb7422f1966afbd19cb8ef6282
Suchita Patil <suchita4004@gmail.com>
2026-09-25 21:57:07 +0530

    ✅ CLGOPJP35 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-25

---
+++ codechef/medium/CLGOPJP35/solution.cpp
@@ -0,0 +1,16 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		int a = -50;
+		int b = 40;
+		int sum=a+b;
+		int product=a*b;
+		int quotined=a/b;
+		System.out.println(sum);
+		System.out.println(product);
+		System.out.println(quotined);
+		// update your code belowhis line
+		
+	}
+}
