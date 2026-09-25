c207de11ef6884cc94273ee5ebdd82347b7ba7a2
Suchita Patil <suchita4004@gmail.com>
2026-09-25 21:56:21 +0530

    ✅ CLGOPJP31 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-25

---
+++ codechef/medium/CLGOPJP31/solution.java
@@ -0,0 +1,10 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+        boolean x=true;
+        boolean y=false;
+        System.out.println(x);
+        System.out.println(y);
+	}
+}
