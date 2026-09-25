7870646723ae711105903e05e5cf35b90cad6fce
Suchita Patil <suchita4004@gmail.com>
2026-09-25 19:26:03 +0530

    ✅ PRACPROB13 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-25

---
+++ codechef/medium/PRACPROB13/solution.cpp
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class PassingScores {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt(); // Number of scores
+        int[] scores = new int[N];
+        int sum = 0;
+        
+        // Your code goes here 
+        
+        for (int i=0;i<N; i++){
+            scores[i]= sc.nextInt();
+            sum +=scores[i];
+            
+        }
+        
+        double average =(double) sum/N;
+         for (int i =0; i <N;i++){
+             if (scores[i] >= average){
+                 System.out.println(scores[i] + " ");
+             }
+         }
+        
+          
+      sc.close();  
+    }
+}
