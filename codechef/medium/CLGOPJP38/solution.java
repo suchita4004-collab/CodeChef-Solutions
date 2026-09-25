216ee7dea5544f3cd6fdee4623236753d0ce8bfb
Suchita Patil <suchita4004@gmail.com>
2026-09-25 21:57:45 +0530

    ✅ CLGOPJP38 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-25

---
+++ codechef/medium/CLGOPJP38/solution.java
@@ -0,0 +1,12 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	    int length = 6;
+	    int div= length /=3;
+	    System.out.println (div);
+	    
+	   // Update your code below this line
+		
+	}
+}
