061d058ab36815bb09fb926bfb0abe126e13daf7
Suchita Patil <suchita4004@gmail.com>
2026-09-25 21:57:35 +0530

    ✅ CLGOPJP37 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-25

---
+++ codechef/medium/CLGOPJP37/solution.java
@@ -0,0 +1,12 @@
+class Codechef
+{
+    public static void main(String[] args)
+    {
+        // Update your code below this line
+        int a = 21;
+        int b = 40;
+        int sum =a+b;
+        System.out.println(sum);
+        
+    }
+}
