8d67f4e7a68c90ce18513d17ef0d7c11cf69df6f
Suchita Patil <suchita4004@gmail.com>
2026-09-25 21:56:30 +0530

    ✅ CLGOPJP32 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-25

---
+++ codechef/medium/CLGOPJP32/solution.java
@@ -0,0 +1,12 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	    // Update the blanks in the code below
+		int s = 14;
+        int area = s * s ;
+        int cost = 7 * area ;
+        System.out.println(area);
+        System.out.println(cost);
+	}
+}
