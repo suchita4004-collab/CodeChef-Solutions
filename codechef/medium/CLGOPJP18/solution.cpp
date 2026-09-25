93c73606992ddfa2e8a2eae661098c9a61963560
Suchita Patil <suchita4004@gmail.com>
2026-09-25 21:54:54 +0530

    ✅ CLGOPJP18 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-25

---
+++ codechef/medium/CLGOPJP18/solution.cpp
@@ -0,0 +1,9 @@
+// Write complete code to print the sum of 12 and 7
+// Be careful about uppercase and lowercase letters
+class Codechef
+{
+    public static void main (String[] args)
+    {
+        System.out.println(12+7);
+    }
+}
