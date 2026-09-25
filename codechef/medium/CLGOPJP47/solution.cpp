991f6157e2c711969d6fc5faf821223120baed68
Suchita Patil <suchita4004@gmail.com>
2026-09-25 21:59:59 +0530

    ✅ CLGOPJP47 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-25

---
+++ codechef/medium/CLGOPJP47/solution.cpp
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 15;
+        // Update the blank in code below
+
+        System.out.println((num % 2 == 1) ? value_if_0 : value_if_1);
+    }
+}
